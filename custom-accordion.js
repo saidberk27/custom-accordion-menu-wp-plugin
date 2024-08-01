ca0dbc009bec05dd7a32151231cd215382b4b6ea
Said Berk <csaidberk@gmail.com>
2024-08-01 14:35:30 +0300

    Pop UP Icons being shown.

--- custom-accordion.js
+++ custom-accordion.js
@@ -6,7 +6,6 @@ jQuery(document).ready(function ($) {
     $('.product-item').click(function () {
         var productId = $(this).data('product-id');
 
-        // Pop-up'ı hemen aç
         $('#product-popup').fadeIn();
         $('.circular-progress').show();
         $('#popup-image').hide();
@@ -19,10 +18,33 @@ jQuery(document).ready(function ($) {
                 product_id: productId
             },
             success: function (response) {
+                console.log("Ajax response:", response); // Tüm yanıtı konsola yazdır
                 if (response.success && response.data) {
                     $('#popup-title').text(response.data.title || '');
                     $('#popup-description').html(response.data.description || '');
                     $('#popup-price').html(response.data.price || '');
+
+                    // Attribute ikonlarını ekle
+                    var attributeIcons = '';
+                    if (response.data.attributes && response.data.attributes.alerjen) {
+                        console.log("Alerjenler:", response.data.attributes.alerjen);
+
+                        // Alerjen string'ini '|' karakterine göre ayır ve boşlukları temizle
+                        var alerjenler = response.data.attributes.alerjen[0].split('|').map(function (item) {
+                            return item.trim();
+                        });
+
+                        alerjenler.forEach(function (alerjen) {
+                            var iconPath = getAttributeIconPath(alerjen);
+                            console.log("Alerjen:", alerjen, "Icon Path:", iconPath);
+                            if (iconPath) {
+                                attributeIcons += '<div class="attribute-icon-container">' +
+                                    '<img src="' + iconPath + '" alt="' + alerjen + '" class="attribute-icon">' + '</div>';
+                            }
+                        });
+                    }
+                    console.log("Attribute Icons HTML:", attributeIcons);
+                    $('#popup-attributes').html(attributeIcons);
 
                     // Resmi yükle ve progress bar'ı güncelle
                     var img = new Image();
@@ -38,7 +60,8 @@ jQuery(document).ready(function ($) {
                         if (progress > 100) {
                             clearInterval(progressInterval);
                         } else {
-                            $('.circular-progress').css('background', `conic-gradient(#e21833 ${progress * 3.6}deg, #ededed 0deg)`); $('.inner').text(progress + '%');
+                            $('.circular-progress').css('background', `conic-gradient(#e21833 ${progress * 3.6}deg, #ededed 0deg)`);
+                            $('.inner').text(progress + '%');
                         }
                     }, 100);
                 } else {
@@ -64,4 +87,23 @@ jQuery(document).ready(function ($) {
             $('#product-popup').fadeOut();
         }
     });
+
+    // Attribute'a göre ikon yolunu döndüren yardımcı fonksiyon
+    function getAttributeIconPath(attribute) {
+        var iconPaths = {
+            'aci': 'https://caddeparklounge.com/wp-content/uploads/2024/08/Alerjen_Aci.png',
+            'mantar': 'https://caddeparklounge.com/wp-content/uploads/2024/08/mantar-250x250-1.png',
+            'alkol': 'https://caddeparklounge.com/wp-content/uploads/2024/08/alerjen_alkol.png',
+            'gluten': 'https://caddeparklounge.com/wp-content/uploads/2024/08/Alerjen_Gluten.png',
+            'hardal': 'https://caddeparklounge.com/wp-content/uploads/2024/08/Alerjen_Hardal.png',
+            'kereviz': 'https://caddeparklounge.com/wp-content/uploads/2024/08/Alerjen_Kereviz.png',
+            'susam': 'https://caddeparklounge.com/wp-content/uploads/2024/08/Alerjen_Susam.png',
+            'sut': 'https://caddeparklounge.com/wp-content/uploads/2024/08/Alerjen_Sut.png',
+            'yumurta': 'https://caddeparklounge.com/wp-content/uploads/2024/08/Alerjen_Yumurta.png',
+            'kabuk': 'https://caddeparklounge.com/wp-content/uploads/2024/08/Kabuklu_Deniz_Urunu-250x250-1.png',
+            'kukurt': 'https://caddeparklounge.com/wp-content/uploads/2024/08/Alerjen_Kukurt.png',
+            'soya': 'https://caddeparklounge.com/wp-content/uploads/2024/08/alerjen_soya.png'
+        };
+        return iconPaths[attribute.toLowerCase()] || null;
+    }
 });
--- custom-product-accordion.php
+++ custom-product-accordion.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: HGT AJANS AKORDİYON
 Description: HGT AJANS MENÜ AKORDİYONU
-Version: 1.3
+Version: 1.4
 Author: Said Berk
 */
 
@@ -36,16 +36,28 @@ function custom_product_accordion_shortcode() {
                         $products = new WP_Query($args);
                         if ($products->have_posts()) :
                             while ($products->have_posts()) : $products->the_post();
-                                global $product;
-                                ?>
-                                <div class="product-item" data-product-id="<?php echo get_the_ID(); ?>">
-                                    <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'medium'); ?>" alt="<?php the_title(); ?>">
-                                    <h3><?php the_title(); ?></h3>
-                                    <p class="price"><?php echo $product->get_price_html(); ?></p>
-                                    <p class="description"><?php echo wp_trim_words(get_the_excerpt(), 20); ?></p>
+                            global $product;
+                            ?>
+                            <div class="product-item" data-product-id="<?php echo get_the_ID(); ?>">
+                                <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'medium'); ?>" alt="<?php the_title(); ?>">
+                                <h3><?php the_title(); ?></h3>
+                                <p class="price"><?php echo $product->get_price_html(); ?></p>
+                                <p class="description"><?php echo wp_trim_words(get_the_excerpt(), 20); ?></p>
+                                <div class="product-allergens">
+                                    <?php
+                                    $allergens = $product->get_attribute('pa_alerjen');
+                                    if (!empty($allergens)) {
+                                        $allergen_array = explode('|', $allergens);
+                                        foreach ($allergen_array as $allergen) {
+                                            $allergen = trim($allergen);
+                                            echo '<img src="' . get_allergen_icon_url($allergen) . '" alt="' . $allergen . '" class="allergen-icon" title="' . $allergen . '">';
+                                        }
+                                    }
+                                    ?>
                                 </div>
-                                <?php
-                            endwhile;
+                            </div>
+                            <?php
+                        endwhile;
                         endif;
                         wp_reset_postdata();
                         ?>
@@ -56,18 +68,19 @@ function custom_product_accordion_shortcode() {
     </div>
 
     <div id="product-popup" class="product-popup">
-    <div class="product-popup-content">
-        <span class="close-popup">&times;</span>
-        <div class="image-container">
-            <div class="circular-progress">
-                <div class="inner"></div>
+        <div class="product-popup-content">
+            <span class="close-popup">&times;</span>
+            <div class="image-container">
+                <div class="circular-progress">
+                    <div class="inner"></div>
+                </div>
+                <img id="popup-image" src="" alt="Product Image">
             </div>
-            <img id="popup-image" src="" alt="Product Image">
-        </div>
-        <div class="product-info">
-            <h2 id="popup-title"></h2>
-            <p id="popup-description"></p>
-            <p id="popup-price"></p>
+            <div class="product-info">
+                <h2 id="popup-title"></h2>
+                <div id="popup-attributes"></div>
+                <p id="popup-description"></p>
+                <p id="popup-price"></p>
             </div>
         </div>
     </div>
@@ -78,7 +91,7 @@ add_shortcode('product_accordion', 'custom_product_accordion_shortcode');
 
 function enqueue_custom_accordion_scripts() {
     wp_enqueue_style('custom-accordion-style', plugins_url('custom-accordion.css', __FILE__));
-    wp_enqueue_script('custom-accordion-script', plugins_url('custom-accordion.js', __FILE__), array('jquery'), '1.1', true);
+    wp_enqueue_script('custom-accordion-script', plugins_url('custom-accordion.js', __FILE__), array('jquery'), '1.2', true);
     wp_localize_script('custom-accordion-script', 'ajax_object', array('ajax_url' => admin_url('admin-ajax.php')));
 }
 add_action('wp_enqueue_scripts', 'enqueue_custom_accordion_scripts');
@@ -92,14 +105,64 @@ function get_product_details() {
         return;
     }
 
+    $attributes = array();
+    $product_attributes = $product->get_attributes();
+    
+    foreach ($product_attributes as $attribute_name => $attribute) {
+        if ($attribute_name === 'pa_alerjen' || $attribute_name === 'alerjen') {
+            if ($attribute['is_taxonomy']) {
+                $attribute_terms = wp_get_post_terms($product_id, $attribute_name, array('fields' => 'names'));
+                if (!is_wp_error($attribute_terms)) {
+                    $attributes['alerjen'] = $attribute_terms;
+                }
+            } else {
+                $attributes['alerjen'] = explode(', ', $product->get_attribute($attribute_name));
+            }
+        }
+    }
+
+    // Eğer özel ürün niteliği olarak tanımlanmışsa
+    if (empty($attributes['alerjen'])) {
+        $all_attributes = $product->get_attributes();
+        foreach ($all_attributes as $attr_name => $attr) {
+            if (strtolower($attr_name) === 'alerjen') {
+                $attributes['alerjen'] = explode('|', $attr->get_options()[0]);
+                break;
+            }
+        }
+    }
+
     $response = array(
         'title' => $product->get_name(),
         'price' => $product->get_price_html(),
         'description' => $product->get_short_description(),
-        'image' => get_the_post_thumbnail_url($product_id, 'full')
+        'image' => get_the_post_thumbnail_url($product_id, 'full'),
+        'attributes' => $attributes
     );
+
+    error_log('Product Details Response: ' . print_r($response, true));
 
     wp_send_json_success($response);
 }
+
+function get_allergen_icon_url($allergen) {
+    $icon_paths = [
+        'ACI' => 'https://caddeparklounge.com/wp-content/uploads/2024/08/Alerjen_Aci.png',
+        'MANTAR' => 'https://caddeparklounge.com/wp-content/uploads/2024/08/mantar-250x250-1.png',
+        'ALKOL' => 'https://caddeparklounge.com/wp-content/uploads/2024/08/alerjen_alkol.png',
+        'GLUTEN' => 'https://caddeparklounge.com/wp-content/uploads/2024/08/Alerjen_Gluten.png',
+        'HARDAL' => 'https://caddeparklounge.com/wp-content/uploads/2024/08/Alerjen_Hardal.png',
+        'KEREVIZ' => 'https://caddeparklounge.com/wp-content/uploads/2024/08/Alerjen_Kereviz.png',
+        'SUSAM' => 'https://caddeparklounge.com/wp-content/uploads/2024/08/Alerjen_Susam.png',
+        'SUT' => 'https://caddeparklounge.com/wp-content/uploads/2024/08/Alerjen_Sut.png',
+        'YUMURTA' => 'https://caddeparklounge.com/wp-content/uploads/2024/08/Alerjen_Yumurta.png',
+        'KABUK' => 'https://caddeparklounge.com/wp-content/uploads/2024/08/Alerjen_Kabuklu.png',
+        'KUKURT' => 'https://caddeparklounge.com/wp-content/uploads/2024/08/Alerjen_Kukurt.png',
+        'SOYA' => 'https://caddeparklounge.com/wp-content/uploads/2024/08/alerjen_soya.png'
+    ];
+
+    return isset($icon_paths[strtoupper($allergen)]) ? $icon_paths[strtoupper($allergen)] : '';
+}
+
 add_action('wp_ajax_get_product_details', 'get_product_details');
-add_action('wp_ajax_nopriv_get_product_details', 'get_product_details');
+add_action('wp_ajax_nopriv_get_product_details', 'get_product_details');    
